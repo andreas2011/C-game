1bafb8bd0e830ce205722cd109bf456134b9e64d
andreas2011 <137012530@qq.com>
2019-11-30 23:41:32 +0100

    Add files via upload

---
+++ mp18_205-master/controller.h
@@ -0,0 +1,45 @@
+#ifndef CONTROLLER_H
+#define CONTROLLER_H
+
+#include "model.h"
+#include "gameoverdialog.h"
+#include "congratsdialog.h"
+#include "noenemydialog.h"
+#include <QObject>
+#include <iostream>
+#include <QDebug>
+
+
+enum DIRECTION{UP, DOWN, LEFT, RIGHT};
+
+class Controller: public QObject
+{
+    Q_OBJECT
+public:
+    Controller();
+    std::shared_ptr<Model> myModel;
+    int checkTile(int x, int y);
+    void createModel();
+    int meetEnemy(int id);
+    int meetHealthPack(int id);
+    int meetGrassTile();
+    int meetRiverTile();
+    void moveProtagonist(int x, int y);
+
+signals:
+    void moveProtagonistView(int x,int y);
+    void enemySignal(int x, int y);
+    void poisonLeakedSignal(int x, int y);
+    void gameover();
+    void congrats();
+    void meetEnemyTerminal();
+    void meetHealthPackTerminal();
+
+public slots:
+    void moveProtagonist(DIRECTION d);
+    void gameoverdialog();
+    void congratsdialog();
+    void noenemydialog();
+};
+
+#endif // CONTROLLER_H
